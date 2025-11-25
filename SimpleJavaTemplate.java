0222141170139794796206cd6c9cdaec22731aa1
Jared Komoroski <jkomoroski@alianza.com>
2025-11-25 08:50:20 -0700

    Add a dependency

---
+++ SimpleJavaTemplate.java
@@ -0,0 +1,9 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+Logger log = LoggerFactory.getLogger(this.getClass());
+
+void main(String[] args) throws Exception {
+    log.info("Hello World");
+}
